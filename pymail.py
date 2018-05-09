01cae8bd4b1d2fd190c47d21366f8732127e121e
Justin Branco <justin.brancode@gmail.com>
2018-05-09 17:23:52 -0400

    Initial commit

---
+++ pymail.py
@@ -0,0 +1,16 @@
+import smtplib
+
+host = 'smtp.gmail.com'
+port = 587
+username = 'Your email address'
+password = 'Your password'
+
+from_email = username
+to_list = ['your destination email addresses']
+
+email_conn = smtplib.SMTP(host, port)
+email_conn.ehlo()
+email_conn.starttls()
+email_conn.login(username, password)
+email_conn.sendmail(from_email, to_list, "Your message here")
+email_conn.quit()
